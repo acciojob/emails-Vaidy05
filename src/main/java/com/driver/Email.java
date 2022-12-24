2a2c28eb323875b06fdaee2a4d4051552a829f1c
Vaidyanathan Valliappan <vaidyanathan589@gmail.com>
2022-12-24 18:14:40 +0530

    Email

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -25,5 +25,39 @@ public class Email {
         // 3. It contains at least one lowercase letter
         // 4. It contains at least one digit
         // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        if(oldPassword==password){
+            if(newPassword.length()>7){
+                int uppercase = 0;
+                int lowercase = 0;
+                int digit = 0;
+                int specialchar = 0;
+                for(int i=0;i<newPassword.length();i++){
+                    if(newPassword.charAt(i)>=65 && newPassword.charAt(i)<=90)
+                        uppercase++;
+                    else if(newPassword.charAt(i)>=97 && newPassword.charAt(i)<=122)
+                        lowercase++;
+                    else if(Character.isDigit(newPassword.charAt(i)))
+                        digit++;
+                    else
+                        specialchar++;
+                }
+                if(uppercase>0 && lowercase>0 && digit>0 && specialchar>0) {
+                    this.password=newPassword;
+                    System.out.println("Password successfully changed");
+                }
+                else {
+                    System.out.println("New Password should have at least one uppercase, one lowercase, one digit and one special character ");
+                    return;
+                }
+            }
+            else{
+                System.out.println("Length of the new password should be more than or equal to 8 character");
+                return;
+            }
+        }
+        else{
+            System.out.println("Password doesn't match");
+            return;
+        }
     }
 }
